e400d4fed8920b645db6152555bd6c15a6dd0073
Mike Grigorov <mikesg@abv.bg>
2014-08-29 16:51:49 +0300

    Made running a class variable of spawner

--- lib/united_workers/worker_spawner.rb
+++ lib/united_workers/worker_spawner.rb
@@ -3,6 +3,7 @@
 module UnitedWorkers
   class WorkerSpawner
     def self.start(bootstrap, instance_count, channel_id)
+      @running = true
       instance_count.times { launch(bootstrap, channel_id) }
     end
 
@@ -20,16 +21,15 @@ module UnitedWorkers
     def self.register(bootstrap, pid, channel_id)
       UnitedWorkers::Queue.new_fanout_queue(channel_id).tap do |queue|
         queue.subscribe do |_, __, message|
-          running = true
           m = UnitedWorkers::Queue.unpack(message)
-          if running && m[:type] == :task_end && m[:status] == :killed && m[:pid] == pid && m[:task_id] == "process_#{pid}"
+          if @running && m[:type] == :task_end && m[:status] == :killed && m[:pid] == pid && m[:task_id] == "process_#{pid}"
             UnitedWorkers::Logger.log("A worker with PID #{pid} was killed. Restarting")
             launch(bootstrap, channel_id)
             queue.channel.close
             queue.channel.connection.close
           elsif m[:type] == :shutdown
-            p "Shutting down spawner"
-            running = false
+            UnitedWorkers::Logger.log("Shutting down spawner")
+            @running = false
           end
         end
       end
